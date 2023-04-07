0b9ea756f3bac264fc1108fa71fa214a9957a0b2
mozes20 <htcmokka@gmail.com>
2023-04-07 21:17:54 +0200

    projectbase

---
+++ src/app/app-routing-module/app-routing-module.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "../home/home.component";
+import {LoginComponent} from "../login/login.component";
+import {AuthGuard} from "../auth.guard";
+
+const routes: Routes = [
+  { path: '', component: HomeComponent,canActivate: [AuthGuard] },
+  { path: 'profile', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {httpInterceptorProviders} from "./http-interceptors";
+import {AuthService} from "./auth.service";
+import {ReactiveFormsModule} from "@angular/forms";
+import {UserService} from "./user.service";
+import {RequestService} from "./request.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from "./app-routing-module/app-routing-module.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders,AuthService,UserService,RequestService,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
